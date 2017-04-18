b3067c1ec374323c39733bfb101d7da080afc79d
mcourtne <Courtney.Matt.R@gmail.com>
2017-04-18 14:01:35 -0700

    added BTapsTimer class

--- btaps-node.js
+++ btaps-node.js
@@ -3,7 +3,8 @@ var RSVP = require('rsvp');
 var BTaps = function(btaddr){
     this.socket = new (require('bluetooth-serial-port')).BluetoothSerialPort();
     this.btaddr = btaddr; // bluetooth address
-    this.timers = [];  // array of timer indices
+    this.timers = {};  // array of timer indices
+    this.enabled = false;
 };
 
 BTaps.prototype = {
@@ -19,12 +20,16 @@ BTaps.prototype = {
             that.socket.findSerialPortChannel(that.btaddr, function(channel){
                 // channel found, connect to it
                 that.socket.connect(that.btaddr,channel,function(){
-                    
-                    // TODO: get timers on connected device
-                    // TODO: listen for socket close to remove timers                    
-                    // resolve promise
-                    resolve();
-                
+
+                    // listen for disconnect to remove timers                    
+                    that.socket.on('closed',function(){
+                      that.timers = {};
+                      that.enabled = false;
+                    });  
+ 
+                    // resolve promise after updating state from device
+                    that.__updateState().then(resolve,reject);
+ 
                 // connection rejected
                 }, function(err){
                     reject(err);
@@ -43,21 +48,15 @@ BTaps.prototype = {
 
     // disconnect from bluetooth
     disconnect: function(){
-            // only attempt disconnect if socket is open
-            if(this.socket.isOpen()){
-                this.socket.close();
-            }
+      this.socket.close();
     },
 
     // set the state of the switch (On/Off)
-    setSwitchState: function(enabled){
-        // bind promise to BTaps object 
-        var that = this;
+    setSwitch: function(enabled){
         
-        // promise for setting switch state
-        var promise = new RSVP.Promise(function(resolve,reject){
-            
+            var write = RSVP.denodeify(this.socket.write.bind(this.socket));
             var payload;
+
             if(enabled){
                 // turn On
                 payload = new Buffer([0xcc,0xaa,0x03,0x01,0x01,0x01]);
@@ -65,21 +64,178 @@ BTaps.prototype = {
                 // turn Off
                 payload = new Buffer([0xcc,0xaa,0x03,0x01,0x01,0x00]);
             }
-            
-            that.socket.write(payload,function(err,bytesWritten){
-                if(err){
-                    reject(err);
-                }else{
-                    resolve();
-                }
-            });
+
+            return write(payload);
+    },
+
+    __setTimer: function(timer,create){
+      var that = this;
+      var promise = new RSVP.Promise(function(resolve,reject){
+    
+        if(!(timer instanceof BTapsTimer)){          
+          reject(new Error('Timer argument in '+create?'createTimer()':'modifyTimer()'+'must be a BTapsTimer object.'));
+        }
+       
+        var write = RSVP.denodeify(that.socket.write);  
+        var startTime = timer.getStartTime_badHex();
+        var endTime = timer.getEndTime_badHex();
+        var createByte;
+        
+        if(create){
+          createByte = 0x20;
+        }else{
+          createByte = 0x03;
+        }
+        
+        var packet = new Buffer([0xcc,0xaa,0x1a,createByte,0x01,timer.id,
+                                timer.repeatDayByte, startTime[0], startTime[1],
+                                endTime[0], endTime[1], timer.enabled], 29);
+       
+        write(packet).then(function(){ 
+          that.timers[timer.id] = timer;
+          resolve();
+        },function(reason){
+          reject(reason);
         });
 
-        return promise;
-    }
+      });
 
-    // TODO: Timer functions
+      return promise;
+    },
+
+    createTimer: function(timer){
+        // forward promise
+        return this.__setTimer(timer,true);
+    },
+    
+    modifyTimer: function(timer){
+        // forward promise
+        return this.__setTimer(timer,false);
+    },
+
+    deleteTimer: function(timer){
+      var that = this;
+      var promise = new RSVP.Promise(function(resolve,reject){
+        
+        var write = RSVP.denodeify(that.socket.write);
+        var timer_id;
+        
+        if( timer instanceof BTapsTimer ){
+            timer_id = timer.id;
+        }else{
+            timer_id = timer;
+        }
+        
+        var payload = new Buffer([0xcc,0xaa,0x04,0x19,0x01,0x01,timer_id]);
+        write(payload).then(function(){
+          delete that.timers[timer_id];
+          resolve();
+        },function(reason){
+          reject(reason);
+        });
+      });
+      return promise;
+    },
+ 
+    // method to request timers and enabled state stored on device 
+    getState: function(){
+      
+      var that = this;
+      var promise = new RSVP.Promise(function(resolve,reject){
+        
+        // state request packet
+        var payload = new Buffer([0xcc,0xaa,0x03,0x12,0x01,0x13]);
+        var _timers = {};;
+	var _enabled;
+        var firstResponse = true;     
+  
+        that.socket.on('data',function(buffer){
+          // we need to buffer the data until 0x00 found
+          if(buffer.length == 1 && buffer.readInt8(0) == 0){
+            resolve({enabled:_enabled,timers:_timers});
+          }else{
+            // parse response packet
+            if(firstResponse){
+              // first packet has enabled state of switch
+              _enabled = Boolean(buffer.readInt8(7));
+              firstResponse = false;
+            }else{
+              // each subsequent packet contains a timer
+              var id        = buffer.readInt8(0);
+              var repeats   = buffer.readInt8(1);
+              var startTime = [badHex2dec(buffer.readInt8(2)),
+                               badHex2dec(buffer.readInt8(3))];
+              var endTime   = [badHex2dec(buffer.readInt8(4)),
+                               badHex2dec(buffer.readInt8(5))];
+              var enabled   = Boolean(buffer.readInt8(6));
+              var name      = buffer.toString('utf8',7); 
+            
+              _timers[id]   = new BTapsTimer(id,name,startTime,endTime,enabled);
+              _timers[id].repeatDayByte = repeats;
+            }
+          }
+        });
+
+        that.socket.write(payload,function(err,byteWritten){
+          if(err){
+            reject(err);
+            }
+          // resolved in response parsing
+        });
+
+      });
+
+      return promise;
+    },
+
+    // set state of BTaps object timers and enabled members with values from connected device
+    __updateState: function(){
+      var that = this;
+      var promise = new RSVP.Promise(function(resolve,reject){
+        that.getState().then(function(resp){
+          // set BTaps object members with response values
+          that.timers = resp.timers;
+          that.enabled = resp.enabled;
+          resolve();
+        },function(reason){reject(reason)});
+      });
+      return promise;
+    }
 };
 
-module.exports.BTaps = BTaps;
 
+var BTapsTimer = function(id,name,startTime,endTime,enabled){
+    this.id            = id;         // integer between 0 and 255 (inclusive)
+    this.name          = name;       // string up to 16 characters
+    this.startTime     = startTime;  // 2-tuple: hour,minute
+    this.endTime       = endTime;    // 2-tuple: hour,minute
+    this.enabled       = enabled;    // boolean
+    this.repeatDayByte = 0x00;       // bits 1-7 represent Monday thru Sunday
+                                     // bit 0 always zero 
+};
+
+BTapsTimer.prototype = {
+
+    getStartTime_badHex: function(){
+        return [dec2badHex(this.startTime[0]),dec2badHex(this.startTime[1])];
+    },
+
+    getEndTime_badHex: function(){
+        return [dec2badHex(this.endTime[0]),dec2badHex(this.endTime[1])];
+    }
+};
+
+/* HELPERS */
+// 10 -> 0x10
+var dec2badHex = function(dec){
+    return parseInt('0x'+dec);
+};
+// 0x10 -> 10
+var badHex2dec = function(badHex){
+    return parseInt(badHex.toString(16));
+};
+
+
+
+module.exports.BTaps = BTaps;
+module.exports.BTapsTimer = BTapsTimer;
--- test/test.js
+++ test/test.js
@@ -1,17 +1,22 @@
 var btaps = require('../btaps-node'); 
 var sinon = require('sinon');
 var assert = require('chai').assert; 
+var bufferEqual = require('buffer-equal');
 
 describe("btaps-node",function(){
 
-  var bto = new btaps.BTaps("myAddress");
-  var stub_res = sinon.stub().throws();
-  var stub_err = sinon.stub().throws();
+  var bto;
   var test_chn = 123;
-
+  
   beforeEach(function(){
+    bto = new btaps.BTaps("myAddress");
     // add sinon stubs
-    stub_write = sinon.stub(bto.socket,'write').callsArg(1);
+    stub_write = sinon.stub(bto.socket,'write').callsFake(function(payload,callback){
+      // done response for getState()
+      bto.socket.emit('data',new Buffer([0x00]));
+      // write succeeded
+      callback(false,payload.length);
+    });
     stub_close = sinon.stub(bto.socket,'close');
     stub_isOpen = sinon.stub(bto.socket,'isOpen');
     stub_channelSearchSucceed = sinon.stub(bto.socket,'findSerialPortChannel').callsArgWithAsync(1,test_chn);
@@ -27,12 +32,244 @@ describe("btaps-node",function(){
     stub_connectSucceed.restore();
   });
 
-  it("connect() should search for channels on btaddr provided in constructor",function(){
+  it("connect() should search for channels on btaddr provided in constructor",function(done){
     bto.connect().then(function(){
-      assert.isOk(false)
+      assert.equal(bto.btaddr,stub_channelSearchSucceed.getCalls()[0].args[0]);
+      done();
     },function(reason){
-      assert.equal(bto.btaddr,stub_channelSearchFail.getCalls()[0].args[0]);
+      done("Promise resolved.");
+    }).catch(done);
+  });
+
+  describe("channel search succeeded", function(){
+      
+    it("connect() should attempt to connect to btaddr and found channel",function(done){
+      bto.connect().then(function(){
+        assert.equal(bto.btaddr,stub_connectSucceed.getCalls()[0].args[0]);
+        assert.equal(test_chn,
+                     stub_connectSucceed.getCalls()[0].args[1]);
+        done();
+      },function(){
+        done('Promise resolved.');
+      }).catch(done);
     });
+        
+    describe("connect succeeded",function(){
+
+      var timer = new btaps.BTapsTimer(1,'Test',[10,1],[11,2],true);
+      timer.repeatDayByte = 5;  
+
+      it("disconnect() should close socket",function(){
+          bto.disconnect();
+          sinon.assert.called(stub_close);
+      });
+
+      describe("timers",function(){
+
+        it("getStartTime_badHex()",function(){
+          var ret = timer.getStartTime_badHex();
+          assert.equal(ret[0],0x10);
+          assert.equal(ret[1],0x01);
+        });        
+
+        it("getEndTime_badHex()",function(){
+          var ret = timer.getEndTime_badHex();
+          assert.equal(ret[0],0x11);
+          assert.equal(ret[1],0x02);
+        });
+
+        it("modifyTimer()",function(done){
+          bto.modifyTimer(timer).then(function(){
+            var args = stub_write.getCalls()[0].args;
+            assert.lengthOf(args,2);
+            assert.instanceOf(args[0],Buffer);
+            assert.isTrue(bufferEqual(args[0],
+              new Buffer([0xcc,0xaa,0x1a,0x03,0x01,0x01,0x05,0x10,0x01,0x11,0x02,0x01],29)));
+            done();
+          }).catch(done);
+        });
+
+        it("createTimer()",function(done){
+          bto.createTimer(timer).then(function(){
+            var args = stub_write.getCalls()[0].args;
+            assert.lengthOf(args,2);
+            assert.instanceOf(args[0],Buffer);
+            assert.isTrue(bufferEqual(args[0],
+              new Buffer([0xcc,0xaa,0x1a,0x20,0x01,0x01,0x05,0x10,0x01,0x11,0x02,0x01],29)));
+            done();
+          }).catch(done); 
+        });
+
+        it("deleteTimer(id)",function(done){
+          bto.deleteTimer(timer.id).then(function(){
+            var args = stub_write.getCalls()[0].args;
+            assert.lengthOf(args,2);
+            assert.instanceOf(args[0],Buffer);
+            assert.isTrue(bufferEqual(args[0],
+              new Buffer([0xcc,0xaa,0x04,0x19,0x01,0x01,0x01])));
+            done();
+          }).catch(done);
+        });
+        
+        it("deleteTimer(timer)",function(done){
+          bto.deleteTimer(timer).then(function(){
+            var args = stub_write.getCalls()[0].args;
+            assert.lengthOf(args,2);
+            assert.instanceOf(args[0],Buffer);
+            assert.isTrue(bufferEqual(args[0],
+              new Buffer([0xcc,0xaa,0x04,0x19,0x01,0x01,0x01])));
+            done();
+          }).catch(done);
+        }); 
+
+      });
+
+      describe("state",function(){ 
+        beforeEach(function(){
+          stub_write.restore();
+          stub_writeState = sinon.stub(bto.socket,'write').callsFake(function(payload,callback){
+            // send state of switch
+            this.emit('data',new Buffer([0,0,0,0,0,0,0,0x01]));
+            // send timer: id=1,repeat=5,start=[10,1],end=[11,2],enabled=true,name='test'
+            this.emit('data',new Buffer([0x01,0x05,0x10,0x01,0x11,0x02,0x01,116,101,115,116],23));
+            // end response
+            this.emit('data',new Buffer([0x00]));
+            
+            callback(false,payload.length);
+          });
+        });
+
+        afterEach(function(){
+          stub_writeState.restore();
+        });
+
+        it("connect() should update state on successful connection",function(done){
+          assert.isFalse(bto.enabled);
+          assert.lengthOf(Object.keys(bto.timers),0);
+          bto.connect().then(function(){
+            assert.isTrue(bto.enabled);
+            assert.lengthOf(Object.keys(bto.timers),1);
+            assert.property(bto.timers,'1');
+            done();
+          }).catch(done);
+        });
+        
+        it("getState()",function(done){
+          bto.getState().then(function(obj){
+            assert.isObject(obj);
+
+            //check timers property
+            assert.property(obj,'timers');
+            assert.property(obj.timers,'1');
+            var aTimer = obj.timers[1];
+            assert.instanceOf(aTimer,btaps.BTapsTimer);
+            var eTimer = new btaps.BTapsTimer(1,'test',[10,1],[11,2],true);
+            eTimer.repeatDayByte = 5;  
+            assert.deepEqual(aTimer,eTimer);
+
+            //check enabled property
+            assert.property(obj,'enabled');
+            assert.equal(obj.enabled,true);
+
+            done();
+          }).catch(done);
+        });
+
+      });
+    
+      describe("switch", function() {
+
+        it("should send request over bluetooth serial socket",function(done){
+          bto.setSwitch(true).then(function(){
+            sinon.assert.called(stub_write);
+            arg0 = stub_write.getCalls()[0].args[0];
+            assert.instanceOf(arg0,Buffer);
+            done();
+          },function(reason){done('Promise rejected: '+reason);}).catch(done);
+        });
+        
+        describe('write failed', function(){
+    
+          beforeEach(function(){
+            stub_write.restore();
+            stub_write_fail = sinon.stub(bto.socket,'write').callsArgWith(1,'write failed');
+          });
+
+          afterEach(function(){
+            stub_write.restore();
+          });
+
+          it("createTimer() should forward socket write Errors",function(done){
+            bto.createTimer(timer).then(function(){
+              done('Promise resolved.');
+            },function(reason){
+              assert.equal(reason,'write failed');
+              done(); 
+            }).catch(done);
+          });
+
+          it("modifyTimer() should forward socket write Errors",function(done){
+            bto.modifyTimer(timer).then(function(){
+             done('Promise resolved.');
+            },function(reason){
+              assert.equal(reason,'write failed');
+              done(); 
+            }).catch(done);
+          });
+
+          it("deleteTimer() should forward socket write Errors",function(done){
+            bto.deleteTimer().then(function(){
+              done('Promise resolved.');
+            },function(reason){
+              assert.equal(reason,'write failed');
+              done();
+            }).catch(done);
+          });
+          
+          it("setSwitch() should forward socket write Errors",function(done){
+            bto.setSwitch(false).then(function(){
+              done('Promise resolved.');
+            },function(reason){
+              assert.equal(reason,'write failed');
+              done(); 
+            }).catch(done);
+          });
+          
+          it("getState() should forward socket write Errors",function(done){
+            bto.getState().then(function(){
+              done('Promise resolved.');
+            },function(reason){
+              assert.equal(reason,'write failed');
+              done(); 
+            }).catch(done);
+          });
+ 
+        }); 
+      });
+    });
+    
+    describe("connect failed",function(){
+
+      beforeEach(function(){
+        bto.socket.connect.restore(); 
+        stub_connectFail = sinon.stub(bto.socket,'connect').callsArgWith(3,new Error('connect failed'));
+      });
+
+      afterEach(function(){
+        stub_connectFail.restore();
+      });
+ 
+      it("connect() should reject with Error when socket.connect errors",function(done){
+        bto.connect().then(function(){
+          done('Promise resolved.'); 
+        },function(reason){
+          assert.instanceOf(reason,Error);
+          done();
+        }).catch(done);
+      });
+      
+    });
+
   });
 
   describe("channel search failed", function() {
@@ -46,85 +283,15 @@ describe("btaps-node",function(){
       stub_channelSearchFail.restore();
     });
    
-    it("connect() should reject with Error when channels are not found", function(){
+    it("connect() should reject with Error when channels are not found", function(done){
       bto.connect().then(function(){
-        assert.isOk(false)
+        done('Promise resolved.');
       },function(reason){
         assert.instanceOf(reason,Error);
-      });
+        done();
+      }).catch(done);
     });
       
   });
 
-  describe("channel search succeeded", function(){
-      
-    it("connect() should attempt to connect to btaddr and found channel",function(){
-      bto.connect().then(function(){assert.isOk(false)},function(reason){
-        assert.equal(bto.btaddr,stub_connectFail.getCalls()[0].args[0]);
-        assert.equal(test_chn,
-                     stub_connectFail.getCalls()[0].args[1]);
-      });
-    });
-        
-    describe("connect failed",function(){
-
-      beforeEach(function(){
-        bto.socket.connect.restore(); 
-        stub_connectFail = sinon.stub(bto.socket,'connect').callsArgWith(3,new Error('connect failed'));
-      });
-
-      afterEach(function(){
-        stub_connectFail.restore();
-      });
- 
-      it("connect() should reject with Error when socket.connect errors",function(){
-        bto.connect().then(function(){assert.isOk(false)},function(reason){
-          assert.instanceOf(reason,Error);
-        });
-      });
-      
-    });
-
-    describe("connect succeeded",function(){
-
-      //it("connect() should update timers on successful connection");
-
-      it("disconnect() should close socket",function(){
-        bto.connect().then(function(){
-          bto.disconnect();
-          assert.called(stub_close);
-        },function(){assert.isOk(false);});
-      });
-    
-      describe("switch", function() {
-
-        it("should send request over bluetooth serial socket",function(){
-          bto.setSwitchState(true).then(function(){
-            assert.called(stub_write);
-            arg0 = stub_write.getCalls()[0].args[0];
-            assert.isInstance(arg0,Buffer);
-          },function(){assert.isOK(false);}); 
-        });
-        
-        describe('write failed', function(){
-    
-          beforeEach(function(){
-            bto.socket.write.restore();
-            stub_write_fail = sinon.stub(bto.socket,'write').callsArgWith(1,true);
-          });
-
-          afterEach(function(){
-            bto.socket.write.restore();          
-          });
-
-          it("should forward socket write Errors",function(){
-            bto.setSwitchState(false).then(function(){assert.isOk(false);},function(reason){
-             assert.instanceOf(reason,Error); 
-            });
-          });
- 
-        }); 
-      });
-    });
-  });
 });
